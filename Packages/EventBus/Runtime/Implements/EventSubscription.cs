4e2c47bb6eb5e7363550ce6d31e8f35eea07de58
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2026-01-13 22:22:27 +0900

    イベント購読の実装クラスをより安全な形に修正

--- Packages/EventBus/Runtime/Implements/EventSubscription.cs
+++ Packages/EventBus/Runtime/Implements/EventSubscription.cs
@@ -3,18 +3,27 @@ using PipetteGames.Events.Interfaces;
 
 namespace PipetteGames.Events
 {
-    public class EventSubscription : IEventSubscription
+    internal class EventSubscription : IEventSubscription
     {
-        private readonly Action _unsubscribe;
+        private Action _unsubscribe;
+        private bool _isDisposed;
 
         public EventSubscription(Action unsubscribe)
         {
-            _unsubscribe = unsubscribe;
+            _unsubscribe = unsubscribe ?? throw new ArgumentNullException(nameof(unsubscribe));
+            _isDisposed = false;
         }
 
         public void Dispose()
         {
+            if (_isDisposed)
+            {
+                return;
+            }
+
             _unsubscribe?.Invoke();
+            _unsubscribe = null;
+            _isDisposed = true;
         }
     }
 }
